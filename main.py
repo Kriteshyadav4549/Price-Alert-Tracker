c86249b0eecbc736f96309d4c86043f67eb50d24
Kriteshyadav4549 <137719073+Kriteshyadav4549@users.noreply.github.com>
2024-10-10 21:22:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -81,40 +81,4 @@ if __name__ == "__main__":
     priceAlerter.run(debug=True)
 
 
-#header = {
-#    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36",
-#    "Accept-Language": "en-US,en;q=0.8",
-#    "sec-ch-ua": '"Brave";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
-#    "Accept-Encoding": "gzip, deflate, br",
-#    "X-Forwarded-For": "27.7.81.18",
-#    "Cookie": "PHPSESSID=bf8d54908edd79099800eb54c228045ex-forwarded-proto:https"
-#}
-#ans = False
-#gPrice = 0
-##need = int(input("Enter the target price : "))
-##We use while loop so that we loop until it stops returning captcha and then we get the price
-#while ans==False:
-#    try:
-#        response = requests.get(url, headers=header)
-#        soup = BeautifulSoup(response.content, "html.parser")
-#        price = soup.find(class_="a-price-whole").getText()
-#        #int_price = price.split(".")[0].split(",")
-#        int_price = price.replace(',','')
-#        #Price = int(int_price[0]+int_price[1])
-#        int_price = int_price.replace('.', '')
-#        Price = int(int_price)
-#        print(Price)
-#        gPrice = Price
-#        ans = True
-#    except AttributeError as err:
-#        ans = False
-#
-#myemail = "xassassinffx@gmail.com"
-#password = "sziitqnghzwtxyyr"
-#if gPrice<need:
-#    connection = smtplib.SMTP("smtp.gmail.com")
-#    connection.starttls()
-#    connection.login(myemail, password)
-#    connection.sendmail(from_addr=myemail, to_addrs="latureanish@gmail.com", msg="Price Alert")
-#else:
-#    print("The price is currently higher than target price")
+
